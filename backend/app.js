076863e8c042dcadc6262231342fefe1bfe5c77d
anuj-yadv <anujyadav.gzp730@gmail.com>
2025-03-24 21:52:13 +0530

    Environment setup

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+const app =express()
+import dotenv from "dotenv"
+import { connectDatabase } from "./config/dbConnect.js";
+
+dotenv.config({path: "backend/config/config.env"});
+//connecting to database
+connectDatabase();
+
+//import all routes
+import productRoutes from "./routes/products.js";
+
+app.use("/api/v1",productRoutes);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
