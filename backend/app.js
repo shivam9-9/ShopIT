9d5715426581e7ae8a3f17cd41b0ba72884b8e92
anuj-yadv <anujyadav.gzp730@gmail.com>
2025-03-25 18:07:08 +0530

    added more product controllers

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import { connectDatabase } from "./config/dbConnect.js";
 dotenv.config({path: "backend/config/config.env"});
 //connecting to database
 connectDatabase();
+app.use(express.json());
 
 //import all routes
 import productRoutes from "./routes/products.js";
--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,60 @@
+import Product from "../models/product.js"
+
+//create new Product =>/api/v1/product
 export const getProducts = async(req,res)=>{
+    const products = await Product.find();
     res.status(200).json({
-        Message:"All products",
+        products,
+    });
+};
+
+//create new Product =>/api/v1/admin/products
+export const newProduct = async(req,res)=>{
+    const product= await Product.create(req.body)
+    res.status(200).json({
+product,
+    });
+};
+
+//get single Product detail=>/api/v1/products/:id
+export const getProductDetails = async(req,res)=>{
+    const product = await Product.findById(req?.params?.id);
+    if(!product){
+        return res.status(404).json({
+            error:"Product not found",
+        })
+    }
+    res.status(200).json({
+        product,
+    });
+};
+
+//update Product detail=>/api/v1/products/:id
+export const updateProduct = async(req,res)=>{
+    let product = await Product.findById(req?.params?.id);
+    if(!product){
+        return res.status(404).json({
+            error:"Product not found",
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id,req.body,{new: true});
+    res.status(200).json({
+        product,
+    });
+};
+
+//delete Product detail=>/api/v1/products/:id
+export const deleteProduct = async(req,res)=>{
+    const product = await Product.findById(req?.params?.id);
+    if(!product){
+        return res.status(404).json({
+            error:"Product not found",
+        })
+    }
+
+    await product.deleteOne();
+    res.status(200).json({
+        message:"Product Deleted",
     });
 };
